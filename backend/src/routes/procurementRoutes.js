b5cee48d1efb159af6073d797d008f3628fcdc80
Mohd Zaman <razazaman7861@gmail.com>
2026-08-30 00:34:33 +0530

    Clear main branch

--- backend/src/routes/procurementRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { createProcurement, getProcurement, getProcurementById } from '../controllers/procurementsController.js'
-
-const procurementRouter = express.Router()
-procurementRouter.post('/',createProcurement)
-procurementRouter.get('/',getProcurement)
-procurementRouter.get('/:id',getProcurementById)
-
-export default procurementRouter
