b3bbe94e9a1f347ff1e1aa8a0c29f589af657f33
Mohd Zaman <razazaman7861@gmail.com>
2026-08-29 18:18:08 +0530

    feat: add chat functionality with Gemini integration and restructure backend files

---
+++ backend/src/services/geminiServices.js
@@ -0,0 +1,15 @@
+import { GoogleGenAI } from "@google/genai";
+
+export const generateResponse = async (message) => {
+
+    const ai = new GoogleGenAI({
+        apiKey: process.env.GEMINI_API_KEY
+    });
+
+    const response = await ai.models.generateContent({
+        model: "gemini-3.5-flash-lite",
+        contents: message
+    });
+
+    return response.text;
+};
